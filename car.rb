030f75d8070470c6d303540204190330b942287f
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-01-29 15:33:31 -0500

    created a ruby file
    This is the first ruby file for the race game

---
+++ car.rb
@@ -0,0 +1,51 @@
+class Gamer
+    attr :name
+    def hello
+        puts "Give us your name, Gamer..."
+        @name = name = gets.chomp()
+        respond = "Hello, #{name}..."
+        puts respond
+    end
+end
+
+class Board
+    attr :card
+    def choice(name)
+        questi = "Choose your card, #{name}:\n(a) Spade\n(b) Heart\n(c) Clover\n(d) Diamond"
+        shape = ""
+        ready = false
+
+        puts questi
+        shape = gets.chomp()
+        while !ready
+            if shape == "a" or shape == "spade"
+                @card == "Spade"
+                ready = true
+            elsif shape == "b" or shape == "heart"
+                @card == "Heart"
+                ready = true
+            elsif shape == "c" or shape == "clover"
+                @card = "Clover"
+                ready = true
+            elsif shape == "d" or shape == "diamond"
+                @card = "Diamond"
+                ready = true
+            else
+                puts "Not an option, pick a card"
+                shape = gets.chomp()
+            end
+        end
+            if shape == "a" or shape == "spade"
+                @card == "Spade"
+            elsif shape == "b" or shape == "heart"
+                @card == "Heart"
+            elsif shape == "c" or shape == "clover"
+                @card = "Clover"
+            elsif shape == "d" or shape == "diamond"
+                @card = "Diamond"
+            else
+                puts "Not an option, pick a card"
+                gets.chomp()
+            end
+    end
+end
