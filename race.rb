39bf0181e32354b3004c7d705c544e053d0d7203
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-01-29 15:34:54 -0500

    created race.rb
    For the moment, this is a test simulator on command terminal. Using it to check the mech :3

---
+++ race.rb
@@ -0,0 +1,13 @@
+require_relative "car.rb"
+
+intro = Gamer.new()
+intro.hello()
+
+player_name = intro.name
+
+middle = Board.new()
+middle.choice(player_name)
+
+player_card = middle.card
+
+puts "You have picked the #{player_card}!"
