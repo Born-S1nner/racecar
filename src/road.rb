cacbfe7546a96665507cab08eaa2c93dbd055612
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-02-02 16:20:53 -0500

    Update road.rb
    remove the moving background

--- src/road.rb
+++ src/road.rb
@@ -5,15 +5,7 @@ class Road
         @x = @y = 0.0
         @speed = 3.0
     end
-    def accel
-        @x += @speed
-    end
-    def rever
-        @x -=speed
-        @x -= WIDTH * 2 if @x >= WIDTH
-    end
     def draw
         @background_image.draw(@x, @y, ZOrder::BACKGROUND)
-        @background_image.draw(@x-1534, @y, ZOrder::BACKGROUND)
     end
 end
