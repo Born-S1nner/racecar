9663d497f877ef74bd9f0c461adb96c2287a9d94
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-02-02 16:18:36 -0500

    Update car.rb
    Made it function like a real life car.

--- src/car.rb
+++ src/car.rb
@@ -1,4 +1,3 @@
-
 class GameCar
     attr_reader :x, :y, :angle
     attr_accessor :speed
@@ -7,17 +6,26 @@ class GameCar
         @x = 0.0
         @y = 0.0
         @angle = 0.0
-        @speed_limit = 538
+        @speed_min = 180
+        @speed_limit = 590
     end
     def wrap(x, y)
         @x = x
         @y = y
     end
     def left
-        @y += 1
+        if @y > 150
+            @y -= 4
+        else
+            @y = 150
+        end
     end
     def right
-        @y -= 1
+        if @y < 438
+            @y += 4
+        else
+            @y = 438
+        end
     end
     def accel
         if x < @speed_limit
@@ -26,8 +34,19 @@ class GameCar
             @x = @speed_limit
         end
     end
-    def rever
-        @x -= 1
+    def brake
+        if x > @speed_min
+            @x -= 2
+        else
+            @x = @speed_min
+        end
+    end
+    def move
+        if x < @speed_limit
+            @x += 1
+        else
+            @x = @speed_limit
+        end
     end
     def draw
         @image.draw_rot(@x, @y, ZOrder::CAR, @angle)
