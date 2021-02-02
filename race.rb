b98a95ff4e21f93d4b99200a5b2403fc21a32596
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-02-02 02:50:00 -0500

    Update race.rb
    Main head has been change for the suitable purpose of working

--- race.rb
+++ race.rb
@@ -1,3 +1,5 @@
+require 'gosu'
+
 require_relative "src/car.rb"
 require_relative "src/road.rb"
 require_relative "src/player.rb"
@@ -5,12 +7,39 @@ require_relative "src/player.rb"
 intro = Gamer.new()
 intro.hello()
 
-player_name = intro.name
+module ZOrder
+    BACKGROUND, CAR = *0..5
+end
 
-middle = GameCar.new()
-middle.choice(player_name)
+class Game_set < Gosu::Window
+    def initialize
+        super 640, 480
+        self.caption = "Ruby Racer"
+        @racer = GameCar.new()
+        @racer.wrap(190, 150)
+        @background_image = Road.new()
+    end
+    def update
+        if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
+            @racer.left
+        end
+        if Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
+            @racer.right
+        end
+        if Gosu.button_down? Gosu::KB_A or Gosu::button_down? Gosu::GP_UP
+            @racer.accel
+            @background_image.accel
+        end
+        
+        if Gosu.button_down? Gosu::KB_S or Gosu::button_down? Gosu::GP_DOWN
+            @racer.rever
+            @background_image.rever
+        end
+    end
+    def draw
+        @racer.draw
+        @background_image.draw()
+    end
+end
 
-player_card = middle.card
-
-road = Road.new()
-road.road_setup(player_card)
+Game_set.new.show
