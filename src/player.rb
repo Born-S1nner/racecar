1dc523b2dc22ed388b088ab7f6c1b264e75f1ba3
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-02-04 18:29:15 -0500

    Delete player.rb
    don't need it anyway

--- src/player.rb
+++
@@ -1,9 +0,0 @@
-class Gamer
-    attr :name
-    def hello
-        puts "Give us your name, Gamer..."
-        @name = name = gets.chomp()
-        respond = "Hello, #{name}..."
-        puts respond
-    end
-end
