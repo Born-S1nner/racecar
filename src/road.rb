d1d128dc7d9756d4b23c6ce6b3ebaed79b719ad6
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-02-04 19:03:29 -0500

    Update road.rb
    where the ring file will be since it's part of the road. Still needs work on the rings

--- src/road.rb
+++ src/road.rb
@@ -3,9 +3,32 @@ class Road
     def initialize()
         @background_image = Gosu::Image.new("src/media/background.png", :tileable => true)
         @x = @y = 0.0
-        @speed = 3.0
+        #@speed = 3.0
     end
+=begin
+    def accel
+        @x += @speed
+    end
+    def rever
+        @x -=speed
+        @x -= WIDTH * 2 if @x >= WIDTH
+    end
+=end
     def draw
         @background_image.draw(@x, @y, ZOrder::BACKGROUND)
+    end
+end
+
+class Ring
+    attr_reader :x, :y
+    def initialize(animation)
+        @animation = animation
+        @color = Gosu::Color::YELLOW.dup
+        @x = rand * 640
+        @y = rand * 480
+    end
+    def draw
+        ring = @animation[Gosu.milliseconds / 100 % @animation.size]
+        ring.draw(@x - ring.width / 2.0, @y - ring.height / 2.0, ZOrder::RINGS, 1, 1, @color, :add)
     end
 end
