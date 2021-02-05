710dcea8dcaa81bceb421f38ba1516f6cc7a850a
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-02-04 19:02:10 -0500

    Update car.rb
    move collect rings to this file

--- src/car.rb
+++ src/car.rb
@@ -1,17 +1,16 @@
+
 class GameCar
     attr_reader :x, :y, :angle
     attr_accessor :speed
     def initialize
         @image = Gosu::Image.new("src/media/redcar.png")
-        @x = 0.0
-        @y = 0.0
-        @angle = 0.0
+        @x = @y = @angle = 0.0
         @speed_min = 180
         @speed_limit = 590
+        @score = 0
     end
     def wrap(x, y)
-        @x = x
-        @y = y
+        @x, @y = x, y
     end
     def left
         if @y > 150
@@ -50,5 +49,12 @@ class GameCar
     end
     def draw
         @image.draw_rot(@x, @y, ZOrder::CAR, @angle)
+    end
+    
+    def score
+        @score
+    end
+    def collect_rings(rings)
+        rings.reject! { |ring| Gosu.distance(@x, @y, 51, 47) < 35 }
     end
 end
