e971d6f1c330f12a70b6e1ee783c09fafccccede
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-02-06 17:13:53 -0500

    Update car.rb
    made some changes for the new car

--- src/car.rb
+++ src/car.rb
@@ -1,11 +1,9 @@
 class GameCar
-    attr_reader :x, :y
-    attr_accessor :speed
     def initialize
-        @image = Gosu::Image.new("src/media/redcar.png")
+        @image = Gosu::Image.new("src/media/car.bmp")
         @x = @y = 0
         @speed_min = 40
-        @speed_limit = 590
+        @speed_limit = 593
         @score = 0
     end
     def wrap(x, y)
@@ -19,28 +17,28 @@ class GameCar
         end
     end
     def right
-        if @y < 430
+        if @y < 410
             @y += 4
         else
-            @y = 430
+            @y = 410
         end
     end
     def accel
-        if x < @speed_limit
+        if @x < @speed_limit
             @x += 4
         else
             @x = @speed_limit
         end
     end
     def brake
-        if x > @speed_min
+        if @x > @speed_min
             @x -= 2
         else
             @x = @speed_min
         end
     end
     def move
-        if x > @speed_min
+        if @x > @speed_min
             @x -= 1
         else
             @x = @speed_min
