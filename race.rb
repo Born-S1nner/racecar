9fa7f6f5b4c638c005aa9017d3e1a25262328a4c
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-02-09 14:17:09 -0500

    Update race.rb

--- race.rb
+++ race.rb
@@ -8,41 +8,39 @@ module ZOrder
     BACKGROUND, RINGS, CAR, UI = *0..3
 end
 
-class Game_set < Gosu::Window
+class Race_game < Gosu::Window
     def initialize
         super 640, 480
-        self.caption = "Ruby Racer"
+        self.caption = "Ruby Ring Race"
 
-        @racer = GameCar.new()
-        @racer.wrap(190, 150)
+        @car = GameCar.new()
+        @car.wrap(190, 150)
 
         @background_image = Road.new()
         @font = Gosu::Font.new(20)
 
         @ring_anim = Gosu::Image.load_tiles("src/media/ringer.png", 51, 47)
         @rings = Array.new
-        
+
         @gameOver = Done.new()
     end
     def update
-        @racer.control(@racer)
-        @racer.move()
-        @racer.collect_rings(@rings)
+        @car.control(@car)
+        @car.move()
+        @car.collect_rings(@rings)
         if rand(300) < 4 and @rings.size < 25
             @rings.push(Ring.new(@ring_anim))
         end
-        @gameOver.game_over(@racer.score)
+        @gameOver.game_over(@car.score)
     end
     def draw
         @background_image.draw()
-        @racer.draw
+        @font.draw_text("Score: #{@car.score}", 10, 10, ZOrder::UI, 2.0, 2.0, Gosu::Color::AQUA)
         if @gameOver.end_game == false
-            @racer.draw
+            @car.draw
             @rings.each { |ring| ring.draw}
         else
             @font.draw_text("Game Over", 150, 200, ZOrder::UI, 4.0, 4.0, Gosu::Color::YELLOW)
         end
     end
 end
-
-Game_set.new.show
